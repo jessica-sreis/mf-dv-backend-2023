da8db6168919596614d3baa481ab579c7c72effe
jessica-sreis <144929767+jessica-sreis@users.noreply.github.com>
2024-04-23 12:28:14 -0300

    Crud de usuário

---
+++ mf-dv-backend-2023/Models/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dv_backend_2023.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Obrigatório Informar o nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar a senha")]
+        [DataType(DataType.Password)]   
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar a senha")]
+        public Perfil Perfil { get; set; }
+
+    }
+    public enum Perfil
+    {
+        Admin,
+        User
+    }
+
+
+
+
+}
